a381f9e043b496d75a8f6bc593993132ba256a25
Viet Hoang <92383759+banhbaochay0310@users.noreply.github.com>
2025-09-06 01:44:57 +0700

    feat: add AlbumPage, FriendsActivity and update layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,24 @@ import { Route, Routes } from "react-router-dom";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage.tsx";
 import HomePage from "./pages/home/HomePage.tsx";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout.tsx";
+import ChatPage from "./pages/chat/ChatPage.tsx";
+import AlbumPage from "./pages/album/AlbumPage.tsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/sso-callback"
-          element={
-            <AuthenticateWithRedirectCallback
-              signUpForceRedirectUrl={"auth-callback"}
-            />
-          }
+          element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"auth-callback"}/>}
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/albums/:albumId" element={<AlbumPage />} />
+        </Route>
       </Routes>
     </>
   );
