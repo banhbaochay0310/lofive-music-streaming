ac90f4b3e51d1a7c5dfe8166e8fad600917fbed6
Viet Hoang <92383759+banhbaochay0310@users.noreply.github.com>
2025-09-06 05:20:03 +0700

    feat(frontend): add Homepage

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,44 @@
 import Topbar from "@/components/Topbar";
+import { useMusicStore } from "@/stores/useMusicStore";
+import { useEffect } from "react";
+import FeaturedSection from "./components/FeaturedSection.tsx";
+import { ScrollArea } from "@/components/ui/scroll-area.tsx";
+import SectionGrid from "./components/SectionGrid.tsx";
 
 const HomePage = () => {
+  const {
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
+
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
   return (
-    <div>
+    <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-green-900 to-zinc-800/40">
       <Topbar />
-    </div>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good afternoon
+          </h1>
+          <FeaturedSection />
+        </div>
+        <div className="space-y-8">
+          <SectionGrid title="Made For You" songs={madeForYouSongs} isLoading={isLoading}/>
+          <SectionGrid title="Trending" songs={trendingSongs} isLoading={isLoading}/>
+
+        </div>
+      </ScrollArea>
+    </main>
   );
 };
 
--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -9,9 +9,15 @@ interface MusicStore {
   isLoading: boolean;
   error: string | null;
   currentAlbum: Album | null;
+  featuredSongs: Song[];
+  madeForYouSongs: Song[];
+  trendingSongs: Song[];
 
   fetchAlbums: () => Promise<void>;
   fetchAlbumById: (id: string) => Promise<void>;
+  fetchFeaturedSongs: () => Promise<void>;
+  fetchMadeForYouSongs: () => Promise<void>;
+  fetchTrendingSongs: () => Promise<void>;
 }
 
 export const useMusicStore = create<MusicStore>((set) => ({
@@ -20,13 +26,12 @@ export const useMusicStore = create<MusicStore>((set) => ({
   isLoading: false,
   error: null,
   currentAlbum: null,
+  featuredSongs: [],
+  madeForYouSongs: [],
+  trendingSongs: [],
 
   fetchAlbums: async () => {
-    set({
-      isLoading: true,
-      error: null,
-    });
-
+    set({ isLoading: true, error: null });
     try {
       const response = await axiosInstance.get("/albums");
       set({ albums: response.data });
@@ -36,11 +41,48 @@ export const useMusicStore = create<MusicStore>((set) => ({
       set({ isLoading: false });
     }
   },
-  fetchAlbumById: async (id: string) => {
+
+  fetchAlbumById: async (id) => {
     set({ isLoading: true, error: null });
     try {
       const response = await axiosInstance.get(`/albums/${id}`);
       set({ currentAlbum: response.data });
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  fetchFeaturedSongs: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get("/songs/featured");
+      set({ featuredSongs: response.data });
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  fetchMadeForYouSongs: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get("/songs/made-for-you");
+      set({ madeForYouSongs: response.data });
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  fetchTrendingSongs: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get("/songs/trending");
+      set({ trendingSongs: response.data });
     } catch (error: any) {
       set({ error: error.response.data.message });
     } finally {
