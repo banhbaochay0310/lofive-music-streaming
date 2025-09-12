6e6d6f1723f9e5fdbdc55bde922f693414d20672
Viet Hoang <92383759+banhbaochay0310@users.noreply.github.com>
2025-09-13 05:13:50 +0700

    feat(frontend): update App, axios config, and chat store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import ChatPage from "./pages/chat/ChatPage.tsx";
 import AlbumPage from "./pages/album/AlbumPage.tsx";
 import AdminPage from "./pages/admin/AdminPage.tsx";
 import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage.tsx";
 
 function App() {
   return (
@@ -14,16 +15,20 @@ function App() {
       <Routes>
         <Route
           path="/sso-callback"
-          element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"auth-callback"}/>}
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={"auth-callback"}
+            />
+          }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
         <Route path="/admin" element={<AdminPage />} />
-
 
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/chat" element={<ChatPage />} />
           <Route path="/albums/:albumId" element={<AlbumPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
       <Toaster />
--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -23,7 +23,7 @@ interface ChatStore {
   setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL = "http://localhost:5000"; // Replace with your server URL
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 const socket = io(baseURL, {
   autoConnect: false, // only connect if user is authenticated
