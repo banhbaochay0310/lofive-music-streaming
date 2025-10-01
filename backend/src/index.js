61c3ccafe42458aeba8afa311833bd9bc6705725
Viet Hoang <92383759+banhbaochay0310@users.noreply.github.com>
2025-10-01 17:28:56 +0700

    feat(backend): add route to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,7 @@ import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
+import likedSongRoutes from "./routes/liked-song.route.js";
 
 dotenv.config();
 
@@ -51,6 +52,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+app.use("/api/liked-songs", likedSongRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
